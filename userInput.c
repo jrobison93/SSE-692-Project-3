8ce596568f575077afe71ff4be3b10edfc261c41
Grey Newell <ahteeim@gmail.com>
2015-05-01 14:15:34 -0400

    Added game board draw.

--- userInput.c
+++ userInput.c
@@ -11,13 +11,22 @@ main (int argc, char *argv[])
   noecho ();
   curs_set (FALSE);
 
+  //building the window
+  WINDOW *local_win;
+	int starty = (LINES - 30) / 2;
+	int startx = (COLS - 100) / 2;	
+	local_win = newwin(30, 100, starty, startx);
+        wborder(local_win, '|', '|', '-', '-', '+', '+', '+', '+');
+	mvprintw(starty - 1, startx + 40, "PARALLEL PONG SSE693");
+	refresh();
+	wrefresh(local_win);
 
-  mvprintw (0, 0, "Hello, world!");
+
   int ch;
 
   while (true)
     {
-      clear();
+      //clear();
       ch = getch ();
       switch (ch)
 	{
@@ -26,7 +35,10 @@ main (int argc, char *argv[])
 	  break;
 	case KEY_RIGHT:
 	  printw ("Right arrow is pressed\n");
+	  break;
 	case 'x':
+	  endwin ();
+	  return(1);
 	  break;
 	default:
 	  printw ("The pressed key is ");
@@ -34,12 +46,14 @@ main (int argc, char *argv[])
 	  printw ("%c", ch);
 	  attroff (A_BOLD);
 	}
-      refresh ();
-      sleep (1);
+      //refresh ();
+      //sleep (1);
     }
   endwin ();
 }
 
 void screenDraw() {
+	//Draw paddles
+	//Draw Ball
 	refresh ();
 }
